fd3cc6bbe3153e98143c3b5ec0604452c22c2722
Nikhil Manne <nikhilmanne@Nikhils-MacBook-Air.local>
2025-01-25 14:29:46 +0530

    added home header

--- app/common/HomeHeader.tsx
+++ app/common/HomeHeader.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { View, TextInput, Text } from "react-native";
+import { View, TextInput, Text, Image, TouchableOpacity } from "react-native";
 import homeStyles from "../../sources/styles/HomeStyles";
 
 export default function HomeHeader() {
     return (
         <View style={homeStyles.homeHeaderContainer}>
-            <TextInput />
-            <Text>Hello Home Header</Text>
+            <TouchableOpacity style={homeStyles.homeHeaderSearchInputContainer}>
+                <Image source={require('../../sources/images/small_logo.png')} style={homeStyles.homeSearchIcon}resizeMode="center" />
+                <Text>Search for a product</Text>
+            </TouchableOpacity>    
         </View>
     )
 }
--- sources/styles/HomeStyles.ts
+++ sources/styles/HomeStyles.ts
@@ -1,4 +1,6 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const { width } = Dimensions.get('window');
 
 const homeStyles = StyleSheet.create({
     homeContainer: {
@@ -8,7 +10,27 @@ const homeStyles = StyleSheet.create({
     },
     homeHeaderContainer: {
         backgroundColor: 'white',
+        elevation: 5,
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 10,
+    },
+    homeHeaderSearchInputContainer: {
+        width: width * 0.6,
+        backgroundColor: '#f5f3ed',
+        height: 45,
+        borderRadius: 5,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'   ,
+        padding: 5 
+    },
+    homeSearchIcon: {
+        width: 35, 
+        height: 35,
+        marginRight: 10,
+        opacity: 0.5
     }
 })
 
-export default homeStyles
+export default homeStyles;
